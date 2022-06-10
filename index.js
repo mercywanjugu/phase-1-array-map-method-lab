8f554909def49cbc697125ef67cabdfcdbebc1d7
mercywanjugu <mercy.wanjugu@student.moringaschool.com>
2022-06-10 05:00:32 -0400

    Done.

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+
 const titleCased = () => {
-  return tutorials
+  return tutorials.map((tutorial) => {
+    let words = tutorial.split(' ');
+    return words.map((word) => `${word[0].toUpperCase()}${word.slice(1)}`).join(' ')
+  })
 }
